816d6e73f7d028d707b024fe761d9ba4ec2f08ca
Andre Engelbrecht <andre@teh-node.co.za>
2010-03-09 23:50:51 +0000

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PROJECT_NAME",
+    version="0.1",
+    url="",
+    description="",
+    author="Your Name",
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'setuptools',
+    ],
+)
