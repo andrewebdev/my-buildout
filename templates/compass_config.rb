af3d3fbe3146dc3776d2a68e11a531162458db08
Andre Engelbrecht <andre@teh-node.co.za>
2012-04-02 12:53:27 +0100

    Recently started using compass and sass for css.

---
+++ templates/compass_config.rb
@@ -0,0 +1,20 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+project_path = '/'
+http_path = '/'
+
+sass_dir = '${project-dir}static/sass/'
+css_path = '${project-dir}static/css/'
+images_path = '${project-dir}static/img/'
+javascripts_path = '${project-dir}static/js/'
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
